1b727cebec88831628bc43d84b5deb24db9c783b
Cole Blender <coleblender@gmail.com>
2024-09-11 11:39:36 -0700

    update dockerfile

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,23 @@ import { connect, StringCodec } from "nats";
 
 const sc = StringCodec();
 
+// Publisher
+(async () => {
+  const nc = await connect({
+    servers:
+      process.env.NODE_ENV === "development"
+        ? "nats://localhost:4222"
+        : process.env.NATS_URL,
+  });
+  console.log("Publisher connected to NATS");
+
+  nc.publish("updates", sc.encode("Hello from TypeScript Publisher!"));
+  console.log('Message published to "updates" subject');
+
+  await nc.drain();
+})();
+
+// Subscriber
 (async () => {
   const nc = await connect({
     servers:
