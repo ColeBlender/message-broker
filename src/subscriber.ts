988627fec4e3e933d22b4ac9359aa3756e49f02d
Cole Blender <coleblender@gmail.com>
2024-09-10 22:30:45 -0700

    add publisher and subscriber

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -1,18 +1,14 @@
 import { connect, StringCodec } from "nats";
 
-// Create a codec to encode/decode string messages
 const sc = StringCodec();
 
 (async () => {
-  // Connect to the NATS server
   const nc = await connect({ servers: "nats://localhost:4222" });
   console.log("Subscriber connected to NATS");
 
-  // Subscribe to the "updates" subject
   const sub = nc.subscribe("updates");
   console.log('Subscribed to "updates" subject');
 
-  // Listen for messages
   for await (const msg of sub) {
     console.log(`Received message: ${sc.decode(msg.data)}`);
   }
