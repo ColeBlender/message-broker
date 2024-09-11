5874fc6285ebea98e15e38a584b6cf2dd4f599ba
Cole Blender <coleblender@gmail.com>
2024-09-11 09:31:25 -0700

    update env vars

--- src/publisher.ts
+++ src/publisher.ts
@@ -3,7 +3,12 @@ import { connect, StringCodec } from "nats";
 const sc = StringCodec();
 
 (async () => {
-  const nc = await connect({ servers: "nats://localhost:4222" });
+  const nc = await connect({
+    servers:
+      process.env.NODE_ENV === "development"
+        ? "nats://localhost:4222"
+        : process.env.NATS_URL,
+  });
   console.log("Publisher connected to NATS");
 
   nc.publish("updates", sc.encode("Hello from TypeScript Publisher!"));
--- src/subscriber.ts
+++ src/subscriber.ts
@@ -3,7 +3,12 @@ import { connect, StringCodec } from "nats";
 const sc = StringCodec();
 
 (async () => {
-  const nc = await connect({ servers: "nats://localhost:4222" });
+  const nc = await connect({
+    servers:
+      process.env.NODE_ENV === "development"
+        ? "nats://localhost:4222"
+        : process.env.NATS_URL,
+  });
   console.log("Subscriber connected to NATS");
 
   const sub = nc.subscribe("updates");
